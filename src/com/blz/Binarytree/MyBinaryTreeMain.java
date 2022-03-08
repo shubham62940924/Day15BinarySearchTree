1107d40e8cb1fec68de33c4a9a01666262371b45
first_name <ss329222@gmail.com>
2022-03-08 16:44:28 +0530

    Added:AbilityToAddGivenNumbers

--- src/com/blz/Binarytree/MyBinaryTreeMain.java
+++ src/com/blz/Binarytree/MyBinaryTreeMain.java
@@ -7,10 +7,20 @@ public class MyBinaryTreeMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(14);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 
 		myBinaryTree.searchBST(30);
 		int size = myBinaryTree.getSize();
-		System.out.println("Size of  my binary tree is: " + size);
+		System.out.println("Size of binary tree is: " + size);
 		System.out.println("Binary Tree is :");
 		myBinaryTree.print();
 	}
